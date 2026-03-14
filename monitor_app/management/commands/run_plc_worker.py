22f7b059632efe6a49df38ce061f1643128efc17
Joshua Goetz <joshua.l.goetz@outlook.com>
2026-03-14 10:48:42 -0400

    resetting project

--- monitor_app/management/commands/run_plc_worker.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from monitor_app.plc_worker import PLCWatcher, PLC_MAP
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        self.stdout.write("Starting persistent PLC worker...")
-        watcher = PLCWatcher(PLC_MAP)
-        watcher.run()
--- monitor_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.core.cache import cache
-
-def dashboard(request):
-    data = cache.get('plc_live_state', {})
-    return render(request, 'dashboard.html', {'plc_data': data})
