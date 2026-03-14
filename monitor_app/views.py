9e0e075104b7803c193a9c8aec62fea6f803dca4
Joshua Goetz <joshua.l.goetz@outlook.com>
2026-03-14 12:24:11 -0400

    Fixed Label Issue w/PLC.

--- monitor_app/views.py
+++ monitor_app/views.py
@@ -3,5 +3,18 @@ from django.core.cache import cache
 
 def dashboard(request):
     data = cache.get('plc_live_state', {})
-    return render(request, 'monitor_app/dashboard.html', {'plc_data': data})
+
+    #Have to clean the data from the PLC:
+    data_to_send= {}
+    for name, infor in data.items():
+        clean_values = {
+            'sys_on': info['values'].get('Program:MainProgram.SysOn'),
+            'v1_extended': info['values'].get('Program:MainProgram.Valve1_.Extended'),
+        }
+        data_to_send[name] = {
+            'status': info['status'],
+            'values': clean_values,
+        }
+
+    return render(request, 'monitor_app/dashboard.html', {'plc_data': data_to_send})
 # Create your views here.
