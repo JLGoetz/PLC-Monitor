0e437708d1498b17c9f23fdb954074adfe25fb1f
Joshua Goetz <joshua.l.goetz@outlook.com>
2026-03-14 11:51:08 -0400

    fixed issue with BASE_DIR path.

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+        'LOCATION': BASE_DIR / 'django_cache',
     }
 }
 
